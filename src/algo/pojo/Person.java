30f4e95c14055d2598d056482def9ad6ab8789fa
deepak <deepak@deepak>
2018-03-25 01:07:43 +0530

    Stack Exercise

---
+++ src/algo/pojo/Person.java
@@ -0,0 +1,16 @@
+package algo.pojo;
+
+public class Person {
+	private String name;
+	private String roolNo;
+
+	public Person(String name, String roolNo) {
+		this.name = name;
+		this.roolNo = roolNo;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", roolNo=" + roolNo + "]";
+	}
+}
