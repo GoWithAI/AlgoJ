d34e8556bfc97fe9b0c86f1351539ee39c3f20ad
deepak <dipakpatilcse@gmail.com>
2020-08-26 19:15:37 +0530

    ArrayList Example

--- src/datastructures/DynamicArray.java
+++ src/datastructures/DynamicArray.java
@@ -10,44 +10,83 @@ public class DynamicArray<String> {
 		this.initialCapacity = initialCapacity;
 		data = new Object[initialCapacity];
 	}
-
+	//O(1)
 	public String get(int i) {
-		return null;
+		return (String) data[i];
 	}
-
+	//O(1)
 	public void set(int index, String value) {
-
+		data[index] = value;
 	}
-
+	//O(n)
 	public void insert(int index, String value) {
 
-	}
+		// check size
+		if (size == initialCapacity)
+			resize();
 
+		// Shift element
+		for (int i = size; i > index; i--) {
+			data[i] = data[i - 1];
+		}
+
+		// Insert element on Index
+		data[index] = value;
+		size++;
+	}
+	//O(n)
 	public void delete(int index) {
 
-	}
-
-	public boolean isEmpty() {
-		return true;
-	}
-
-	public boolean contains(String value) {
-		return false;
+		for (int i = index; i < size; i++)
+			data[i] = data[i + 1];
+		
+		data[size--] = null;
+		
 	}
 
 	private void resize() {
 
+		// Create double size array
+		Object[] newData = new Object[initialCapacity * 2];
+
+		// Copy element of Arrays
+		for (int i = 0; i < size; i++) {
+			newData[i] = data[i];
+		}
+
+		data = newData;
+		initialCapacity = initialCapacity * 2;
+	}
+
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+	public boolean contains(String value) {
+
+		for (int i = 0; i < size; i++)
+			if (data[i].equals(value))
+				return true;
+
+		return false;
 	}
 
 	public int size() {
-		return 0;
+		return size;
 	}
 
 	public void print() {
-
+		for (int i = 0; i < size; i++)
+			System.out.println(data[i]);
 	}
 
 	public void add(String value) {
 
+		if (size == initialCapacity) {
+			resize();
+		}
+
+		data[size] = value;
+		size++;
 	}
 }
--- test/test/datastructures/DynamicArrayTest.java
+++ test/test/datastructures/DynamicArrayTest.java
@@ -2,6 +2,7 @@ package test.datastructures;
 
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
 import datastructures.DynamicArray;
 
@@ -15,9 +16,37 @@ public class DynamicArrayTest {
         array = new DynamicArray<String>(2);
     }
     
+    @Test
     public void GETAndSet() {
     	array.set(0, "a");
-    	Assert.assertArrayEquals(0, "a");
+    	Assert.assertEquals("a", array.get(0));
     }
-	
+    
+    @Test
+    public void Insert() throws Exception{
+    	array.add("a"); //0
+    	array.add("b"); //1
+    	array.add("c"); //2
+    	
+    	array.insert(1, "d");
+    	
+    	Assert.assertEquals(4, array.size());
+    	Assert.assertEquals("a", array.get(0));
+    	Assert.assertEquals("d", array.get(1));
+    	Assert.assertEquals("b", array.get(2));
+    	Assert.assertEquals("c", array.get(3));
+    }
+    
+    @Test
+    public void Delete() throws Exception {
+    	array.add("a");
+    	array.add("b");
+    	array.add("c");
+    	
+    	array.delete(1);
+    	    	
+    	Assert.assertEquals(2, array.size());
+    	Assert.assertEquals("a", array.get(0));
+    	Assert.assertEquals("c", array.get(1));
+    }
 }
